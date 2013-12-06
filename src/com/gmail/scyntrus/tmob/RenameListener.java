90e290572f08d50fbe51600d92654ca894f18c57
Scyntrus <Scyntrus@gmail.com>
2013-12-05 21:39:59 -0800

    Carry over town color on rename

--- src/com/gmail/scyntrus/tmob/RenameListener.java
+++ src/com/gmail/scyntrus/tmob/RenameListener.java
@@ -1,6 +1,7 @@
 package com.gmail.scyntrus.tmob;
 
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 import com.palmergames.bukkit.towny.event.RenameTownEvent;
@@ -14,10 +15,14 @@ public class RenameListener implements Listener {
 		this.plugin = plugin;
 	}
 
-	@EventHandler
+	@EventHandler(priority = EventPriority.MONITOR)
 	public void onTownRename(RenameTownEvent e) {
 		String oldName = e.getOldName();
 		Town town = e.getTown();
+		TownyMobs.townColors.put(town.getName(), 
+				TownyMobs.townColors.containsKey(oldName) ? 
+						TownyMobs.townColors.remove(oldName) : 
+							10511680);
 		for (TownyMob tmob : TownyMobs.mobList) {
 			if (tmob.getTownName().equals(oldName)) {
 				tmob.setTown(town);
